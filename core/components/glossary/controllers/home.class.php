ac158d2160f4bb5167641c5d54fb989124e8ad01
Thomas Jakobi <thomas.jakobi@partout.info>
2016-06-19 17:56:11 +0200

    Better default path usage in debug mode

--- core/components/glossary/controllers/home.class.php
+++ core/components/glossary/controllers/home.class.php
@@ -14,20 +14,27 @@ class GlossaryHomeManagerController extends modExtraManagerController
 
     public function initialize()
     {
-        $this->glossary = new GlossaryBase($this->modx);
+        $path = $this->modx->getOption('glossary.core_path', null, $this->modx->getOption('core_path') . 'components/glossary/');
+        $this->glossary = $this->modx->getService('glossary', 'GlossaryBase', $path . '/model/glossary/');
     }
 
     public function loadCustomCssJs()
     {
+        $assetsUrl = $this->glossary->getOption('assetsUrl');
+        $jsUrl = $this->glossary->getOption('jsUrl') . 'mgr/';
+        $jsSourceUrl = $assetsUrl . '../../../source/js/mgr/';
+        $cssUrl = $this->glossary->getOption('cssUrl') . 'mgr/';
+        $cssSourceUrl = $assetsUrl . '../../../source/css/mgr/';
+
         if ($this->glossary->getOption('debug') && ($this->glossary->getOption('assetsUrl') != MODX_ASSETS_URL . 'components/packeteer/')) {
-            $this->addJavascript($this->glossary->getOption('jsUrl') . 'mgr/glossary.js');
-            $this->addJavascript($this->glossary->getOption('jsUrl') . 'mgr/widgets/home.panel.js');
-            $this->addJavascript($this->glossary->getOption('jsUrl') . 'mgr/widgets/terms.grid.js');
-            $this->addLastJavascript($this->glossary->getOption('jsUrl') . 'mgr/sections/home.js');
-            $this->addCss($this->glossary->getOption('cssUrl') . 'mgr/glossary.css');
+            $this->addJavascript($jsSourceUrl . 'glossary.js');
+            $this->addJavascript($jsSourceUrl . 'widgets/home.panel.js');
+            $this->addJavascript($jsSourceUrl . 'widgets/terms.grid.js');
+            $this->addLastJavascript($jsSourceUrl . 'sections/home.js');
+            $this->addCss($cssSourceUrl . 'glossary.css');
         } else {
-            $this->addJavascript($this->glossary->getOption('jsUrl') . 'mgr/glossary.min.js');
-            $this->addCss($this->glossary->getOption('cssUrl') . 'mgr/glossary.min.css?v=v' . $this->glossary->version);
+            $this->addJavascript($jsUrl . 'glossary.min.js');
+            $this->addCss($cssUrl . 'glossary.min.css?v=v' . $this->glossary->version);
         }
         $this->addHtml('<script type="text/javascript">
         Ext.onReady(function() {
